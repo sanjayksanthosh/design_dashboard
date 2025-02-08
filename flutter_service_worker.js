90e2739eef2ea7fd56ff9054249824c660f24364
sanjayCodes <sanjayksanthosh55@gmail.com>
2025-02-08 11:16:09 +0300

    Built web
    Branch: main
    Commit: b2e8afb5ec1f90670d8a2513a0d912b2ee4ffca3
    
    package:peanut 5.1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5e7b51fec9b179ee5d58267af238967d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "120fe7f737957e4deb6175ec2d71585b",
+"flutter_bootstrap.js": "cd42fd75625869a4df9417289c5c3062",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
